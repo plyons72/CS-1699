5f07c5a814ef429583224c2f6f20f651ef1d013e
patrick <plyons72@gmail.com>
2018-04-12 23:04:13 -0400

    Fixed errors, added various includes and finished layout of threads

--- csma.cpp
+++ csma.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
+#include <string>.
 
 using namespace std;
 
-#define NUM_THREADS = 8
+#define NUM_THREADS 8
 
-void call_from_thread(void *)
+void call_from_thread(int tid)
 {
-    printf("Heres a hello message from thread %d")
+    printf("Launched by thread %d\n", tid);
 }
 
 int main(int argc, char *argv[])
@@ -17,15 +21,15 @@ int main(int argc, char *argv[])
     //Launch a group of threads
     for (int i = 0; i < NUM_THREADS; ++i)
     {
-        mobile[i] = std::thread(call_from_thread);
+        mobile[i] = std::thread(call_from_thread, i);
     }
 
-     std::cout << "Launched from the main\n";
+     printf("Launched from the main\n");
 
     //Join the threads with the main thread
     for (int i = 0; i < NUM_THREADS; ++i)
     {
-        t[i].join();
+        mobile[i].join();
     }
 
     return 0;
