2a1ebc15c28aba1d3eb87a4669aeb248ad483e5e
patrick <plyons72@gmail.com>
2018-04-14 11:42:28 -0400

    Added in a shell script to compile and run the program. Added in macros for required variables, and added in RNG based off of a seed granted by current UNIX system time.

--- csma.cpp
+++ csma.cpp
@@ -2,21 +2,30 @@
 #include <thread>
 #include <mutex>
 #include <vector>
-#include <chrono>
-#include <string>.
+#include <ctime>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
 #define NUM_THREADS 8
+#define UNIT_TIME 100
+#define DATA_READY_CHECK_MULTIPLIER 240
+std::mutex csma_lock;
 
 void call_from_thread(int tid)
 {
-    printf("Launched by thread %d\n", tid);
+    // Generates a random number between 1 and 100 to determine the percentage chance of having something to send
+    // To generate a truly random and not just pseudorandom numbers, we seed the rand function with the current Unix System time in main()
+    int probability = (rand() % 100)+ 1;
+    printf("\n\n\nLaunched by thread %d\n", tid);
+    printf("Thread %d generated the number %d\n", tid, probability);
 }
 
 int main(int argc, char *argv[])
 {
     std::thread mobile[NUM_THREADS];
+    srand((int)time(0));
 
     //Launch a group of threads
     for (int i = 0; i < NUM_THREADS; ++i)
